c3b821bbc0e1cf5733402f9ff1682c2e53df1a22
Erikaahide <eahidehernandez@gmail.com>
2025-10-03 19:52:21 -0600

    add wireframe of this portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -184,6 +184,7 @@ export default function App() {
             </p>
             <div style={{ display: "flex", gap: ".5rem", flexWrap: "wrap" }}>
               <a className="btn btn-accent7" href="https://www.figma.com/design/zsiRvaHp8P9v2CB2HxYtHE/Portfolio?node-id=0-1&t=1TMprCo3F1mFlZZx-1" target="_blank" rel="noreferrer">Portfolio</a>
+              <a className="btn btn-accent7" href="https://www.figma.com/design/ZPpJGbMZmsWddn3i0icvXt/Portfolio?node-id=0-1&t=BCGzJieD65UEbvnu-0" target="_blank" rel="noreferrer">Wireframe</a>
             </div>
           </div>
         </Section>
