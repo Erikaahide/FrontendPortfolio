0708590488f6f73a1461af94e74b83091e8c53f1
Erikaahide <eahidehernandez@gmail.com>
2025-10-30 19:02:55 -0600

    Add 404 redirect for React routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import "./i18n";
+
 // 1) tokens PRIMERO
 import "./styles/tokens.css";
 
-// 2) luego tu CSS global
+// 2) CSS global
 import "./index.css";
-import App from './App.jsx'
 import "./App.css";
 
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
